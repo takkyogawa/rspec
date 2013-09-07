a2407a788916f2793e1455196de1c79690730c94
takkyogawa <takkyogawa@gmail.com>
2013-09-07 21:14:42 +0900

    initial

---
+++ message_filter_spec.rb
@@ -0,0 +1,13 @@
+# -*- encoding: utf-8 -*-
+# 勉強用。RSpecの実際の作成過程を学ぶ
+# http://d.hatena.ne.jp/t-wada/20100228/p1　こちらを参考にした
+
+# NGワードの検出を行うMessageFilterクラスをTDDしていく
+
+$:.unshift File.dirname(__FILE__)
+
+require 'rubygems'
+require 'rspec'
+
+describe MssageFilter do
+end
